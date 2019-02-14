51958e6181c38addce0df709efd5a56a115b082b
liudenaxietian <zf201046880416@163.com>
2019-02-14 17:21:31 +0800

    Update CollectAndClassifyFile.py

--- CollectAndClassifyFile.py
+++ CollectAndClassifyFile.py
@@ -2,11 +2,18 @@ import xlrd
 import xlwt
 import os 
 import shutil
+import time
+
+path_a = r'C:\Users\Autobio-A3517\Desktop\sequence.xlsx'
+path_b = r'C:\Users\Autobio-A3517\Desktop\test1.xls'   
+path_c = r'C:\Users\Autobio-A3517\Desktop\test'
+path_d = r'C:\Users\Autobio-A3517\Desktop\test1'
 
 seq = []
-workbook = xlrd.open_workbook('C:\\Users\\Autobio-A3517\\Desktop\\sequence.xlsx')
+workbook = xlrd.open_workbook(path_a)
 table1 = workbook.sheets()[0]
 n_rows = table1.nrows
+
 for i in range(1,n_rows):
     name = table1.cell(i,1).value
     seq.append(name)
@@ -18,37 +25,47 @@ def mkdirs(path):
     else:
         return None
 
-def CreateHyperlink(path):
+def CreateHyperlink(file_path):
+    def set_style(name,height,bold=False):
+        style = xlwt.XFStyle()
+        
+        font = xlwt.Font()
+        font.name = name
+        font.bold = bold
+        font.colour_index = 4
+        font.height = height
+
+        borders= xlwt.Borders()
+        borders.left= 6
+        borders.right= 6
+        borders.top= 6
+        borders.bottom= 6
+
+        style.font = font
+        style.borders = borders
+
+        return style
+
     workbook = xlwt.Workbook()
     worksheet = workbook.add_sheet('123')
-    path = r'C:\Users\Autobio-A3517\Desktop\test1'
-    for file in listdir(path):
-        path1 = os.path.abspath(os.path.join(path,file))
-        link = 'HYPERLINK("%s","%s")'%(str(path),file)
-        worksheet.write(1,1,xlwt.Formula(link))
-    workbook.save(r'C:\Users\Autobio-A3517\Desktop\test1')
+    i= 1
+    for file in os.listdir(file_path):
+        path = os.path.abspath(os.path.join(file_path,file))
+        link='HYPERLINK("%s","%s")'%(str(path),file)
+        worksheet.write(i,1,xlwt.Formula(link),set_style('Times New Roman',220,True))
+        i += 1
+    workbook.save(path_b)
 
 def SearchAndClassify(file_path):
     for file in os.listdir(file_path):
         if os.path.isfile(file_path+'\\'+file):
             for file_name in seq:
                 if file_name in file:
-                    mkdirs('C:\\Users\\Autobio-A3517\\Desktop\\test1\\'+file_name)
-                    shutil.copy(file_path+'\\'+file,'C:\\Users\\Autobio-A3517\\Desktop\\test1\\'+file_name)
-                    CreateHyperlink(file_path)
+                    mkdirs(path_d+'\\'+file_name)
+                    shutil.copy(file_path+'\\'+file,path_d+'\\'+file_name)
+                    CreateHyperlink(path_d)                   
         else:
             SearchAndClassify(file_path+'\\'+file)
 
-''' 将整理的文件路径制作超链接并保存到excel表格中 '''
-# workbook = xlwt.Workbook()
-# worksheet = workbook.add_sheet('123')
-# path = r'C:\\Users\\Autobio-A3517\\Desktop\\456'
-# for file in os.listdir(path):
-#     file_path = os.path.abspath(os.path.join(path,file))
-#     path = os.path.dirname(file_path)
-#     link = 'HYPERLINK("%s","%s")'%(str(path),file)
-#     worksheet.write(1,1,xlwt.Formula(link))
-# workbook.save('C:\\Users\\Autobio-A3517\\Desktop\\test1.xls')
-
 if __name__=='__main__':
-    SearchAndClassify(os.path.abspath('C:\\Users\\Autobio-A3517\\Desktop\\test'))
+    SearchAndClassify(os.path.abspath(path_c))
